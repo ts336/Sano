239ff7ebd1ee68c9c8dc16f2245386c50a30533d
ts336 <22297@apps.eggs.school.nz>
2025-08-11 15:08:34 +1200

    v5.2 - Part of previous commit

--- app/index.tsx
+++ app/index.tsx
@@ -3,149 +3,8 @@ import React, { useState } from 'react';
 import { ActivityIndicator, Button, Image, ImageBackground, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import styles from './styles'; // importing stylesheet
 
-// Home screen, accepts onLogout and onNavigateToInfo props
-const HomeScreen = ({ onLogout, onNavigateToInfo }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={[styles.heading, { paddingLeft: 13 }]}>Good morning{'\n'}Franca</Text>
-      <TouchableOpacity style={styles.imageButton} onPress={onNavigateToInfo}>
-        <ImageBackground
-          source={require('../assets/images/background-appointment.png')} // For local image saved in assets
-          style={styles.imageBackground}
-        >
-        <Text style={styles.imageBackgroundText}>Book an appointment</Text>
-        </ImageBackground>
-      </TouchableOpacity>
-      <Button title="Logout" onPress={onLogout}/>
-    </View>
-  );
-};
-
-// Before you book screen, accepts onGoBackToHome prop
-const BeforeYouBookScreen = ({ onGoBackToHome, onNavigateToAppointmentForm }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={[styles.heading, { paddingLeft: 13 }]}>Before you book...</Text>
-      <Text style={styles.normalColor}>Are you experiencing any of the following symptoms?</Text>
-      <Image
-        source={require('../assets/images/symptoms.png')}
-        style={styles.symptoms}>
-      </Image>
-      <Text style={styles.bold}>If yes, call 111 immediately.</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.backButton} onPress={onGoBackToHome}>
-          <Text style={styles.normalWhite}>Back</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.continueButton}
-          onPress={onNavigateToAppointmentForm}
-        >
-          <Text style={styles.normalWhite}>No, I am not.</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
-
-// Appointment Form Screen
-const AppointmentFormScreen = ({ onGoBackToInfo }) => {
-  // states for radio button, saving to file and error message
-  const [selectedAppointmentType, setSelectedAppointmentType] = useState(null);
-  const [isSaving, setIsSaving] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  // error handling
-  const handleSaveAppointment = async () => {
-    if (!selectedAppointmentType) {
-      setErrorMessage('Please select an appointment type.');
-      return;
-    }
-
-    setIsSaving(true);
-    setErrorMessage('');
-
-    // save to file: options chosen
-    try {
-      const appointmentData = {
-        type: selectedAppointmentType,
-      };
-      
-      const jsonValue = JSON.stringify(appointmentData);
-      
-      await AsyncStorage.setItem('appointment_data', jsonValue);
-    } catch (error) {
-      setErrorMessage(`Failed to save: ${error}`);
-    } finally {
-      setIsSaving(false);
-    }
-  };
-
-  // conditional styling for continue button
-  const isEnabled = selectedAppointmentType !== null;
-  const buttonStyleForm = isEnabled ? styles.continueButton : styles.continueButtonDisabled;
-
-  // if selected appointment type, changes style of button (radio button)
-  // continue button disabled if saving or no appointment type has been selected
-  return ( 
-    <View style={[styles.container, { justifyContent: 'center', alignItems: 'center'}]}>
-      <Text style={[styles.heading, { width: '100%', textAlign: 'center' }]}>Appointment Type</Text>
-      <View style={styles.radioGroupContainer}>
-        <TouchableOpacity
-          style={[
-            styles.radioButton,
-            selectedAppointmentType === 'Virtual' && styles.selectedRadioButton,
-          ]}
-          onPress={() => setSelectedAppointmentType('Virtual')}
-        >
-          <Text style={[
-            styles.radioButtonText,
-            selectedAppointmentType === 'Virtual' && styles.selectedRadioButtonText,
-          ]}>
-            Virtual
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={[
-            styles.radioButton,
-            selectedAppointmentType === 'In-person' && styles.selectedRadioButton,
-          ]}
-          onPress={() => setSelectedAppointmentType('In-person')}
-        >
-          <Text style={[
-            styles.radioButtonText,
-            selectedAppointmentType === 'In-person' && styles.selectedRadioButtonText,
-          ]}>
-            In-person
-          </Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.line} />
-
-      <Text style={[styles.heading, { width: '100%', textAlign: 'center'}]}>Select Practitioner</Text>
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.backButton} onPress={onGoBackToInfo}>
-          <Text style={styles.normalWhite}>Back</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={buttonStyleForm}
-          onPress={handleSaveAppointment}
-          disabled={isSaving || !selectedAppointmentType}
-        >
-          {isSaving ? (
-            <ActivityIndicator color="#4DBBBB" />
-          ) : (
-            <Text style={styles.normalWhite}>Continue</Text>
-          )}
-        </TouchableOpacity>
-      </View>
-      {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
-    </View>
-  );
-};
-
+const fixedUsername = 'test';
+const fixedPassword = '123';
 
 // Login screen, accepts onLoginSuccess prop
 const LoginScreen = ({ onLoginSuccess }) => {
@@ -159,10 +18,10 @@ const LoginScreen = ({ onLoginSuccess }) => {
       setErrorMessage('Email and password cannot be empty.');
     } else {
       // Login logic
-      if (email === 'test' && password === '123') {
+      if (email === fixedUsername && password === fixedPassword) {
         setErrorMessage(''); // Clear any previous error messages
         
-        // Call the function passed in the props to signal that the login was successful
+        // Call the function to signal that the login was successful
         onLoginSuccess();
 
       } else { //Login will only work for set email and password
@@ -207,6 +66,184 @@ const LoginScreen = ({ onLoginSuccess }) => {
         <Text style={styles.loginButtonText}>Sign in</Text>
       </TouchableOpacity>
     </View> //if there is an error, it will be shown with the errorText style, otherwise nothing. onPress handleLogin is called
+  );
+};
+
+// Home screen, accepts onLogout and onNavigateToInfo props
+const HomeScreen = ({ onLogout, onNavigateToInfo }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.heading, { paddingLeft: 13 }]}>Good morning{'\n'}{fixedUsername}</Text>
+      <TouchableOpacity style={styles.imageButton} onPress={onNavigateToInfo}>
+        <ImageBackground
+          source={require('../assets/images/background-appointment.png')} // For local image saved in assets
+          style={styles.imageBackground}
+        >
+        <Text style={styles.imageBackgroundText}>Book an appointment</Text>
+        </ImageBackground>
+      </TouchableOpacity>
+      <Button title="Logout" onPress={onLogout}/>
+    </View>
+  );
+};
+
+// Before you book screen, accepts onGoBackToHome prop
+const BeforeYouBookScreen = ({ onGoBackToHome, onNavigateToAppointmentForm }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.heading, { paddingLeft: 13 }]}>Before you book...</Text>
+      <Text style={styles.normalColor}>Are you experiencing any of the following symptoms?</Text>
+      <Image
+        source={require('../assets/images/symptoms.png')}
+        style={styles.symptoms}>
+      </Image>
+      <Text style={styles.bold}>If yes, call 111 immediately.</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.backButton} onPress={onGoBackToHome}>
+          <Text style={styles.normalWhite}>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.continueButton}
+          onPress={onNavigateToAppointmentForm}
+        >
+          <Text style={styles.normalWhite}>No, I am not.</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+// Appointment Form Screen
+const AppointmentFormScreen = ({ onGoBackToInfo }) => {
+  // states for radio buttons, saving to file and error message
+  const [selectedAppointmentType, setSelectedAppointmentType] = useState(null);
+  const [selectedPractitioner, setSelectedPractitioner] = useState(null);
+  const [isSaving, setIsSaving] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // error handling
+  const handleSaveAppointment = async () => {
+    if (!selectedAppointmentType || !selectedPractitioner) {
+      setErrorMessage('Please select all options before continuing');
+      return;
+    }
+
+    setIsSaving(true);
+    setErrorMessage('');
+
+    // save to file: options chosen
+    try {
+      const appointmentData = {
+        type: selectedAppointmentType,
+        practitioner: selectedPractitioner,
+      };
+      
+      const jsonValue = JSON.stringify(appointmentData);
+      
+      await AsyncStorage.setItem('appointment_data', jsonValue);
+    } catch (error) {
+      setErrorMessage(`Failed to save: ${error.message || error}`);
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
+  // conditional styling for continue button
+  const isEnabled = selectedAppointmentType !== null && selectedPractitioner !== null;
+  const buttonStyleForm = isEnabled ? styles.continueButton : styles.continueButtonDisabled;
+
+  // if selected appointment type, changes style of button (radio button)
+  // continue button disabled if saving or no appointment type has been selected
+  return ( 
+    <View style={[styles.container, { justifyContent: 'center', alignItems: 'center'}]}>
+      <Text style={[styles.heading, { width: '100%', textAlign: 'center' }]}>Appointment Type</Text>
+      <View style={styles.radioGroupContainer}>
+        <TouchableOpacity
+          style={[
+            styles.radioButton,
+            selectedAppointmentType === 'Virtual' && styles.selectedRadioButton,
+          ]}
+          onPress={() => setSelectedAppointmentType('Virtual')}
+        >
+          <Text style={[
+            styles.radioButtonText,
+            selectedAppointmentType === 'Virtual' && styles.selectedRadioButtonText,
+          ]}>
+            Virtual
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.radioButton,
+            selectedAppointmentType === 'In-person' && styles.selectedRadioButton,
+          ]}
+          onPress={() => setSelectedAppointmentType('In-person')}
+        >
+          <Text style={[
+            styles.radioButtonText,
+            selectedAppointmentType === 'In-person' && styles.selectedRadioButtonText,
+          ]}>
+            In-person
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.line} />
+
+      <Text style={[styles.heading, { width: '100%', textAlign: 'center'}]}>Select Practitioner</Text>
+      <View style={styles.radioGroupContainer}>
+        <TouchableOpacity
+          style={[
+            styles.radioButton,
+            selectedPractitioner === 'Dr. Bob' && styles.selectedRadioButton,
+          ]}
+          onPress={() => setSelectedPractitioner('Dr. Bob')}
+        >
+          <Text style={[
+            styles.radioButtonText,
+            selectedPractitioner === 'Dr. Bob' && styles.selectedRadioButtonText,
+          ]}>
+            Dr. Bob
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.radioButton,
+            selectedPractitioner === 'Dr. Jane' && styles.selectedRadioButton,
+          ]}
+          onPress={() => setSelectedPractitioner('Dr. Jane')}
+        >
+          <Text style={[
+            styles.radioButtonText,
+            selectedPractitioner === 'Dr. Jane' && styles.selectedRadioButtonText,
+          ]}>
+            Dr. Jane
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.line} />
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.backButton} onPress={onGoBackToInfo}>
+          <Text style={styles.normalWhite}>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={buttonStyleForm}
+          onPress={handleSaveAppointment}
+          disabled={isSaving || !selectedAppointmentType}
+        >
+          {isSaving ? (
+            <ActivityIndicator color="#4DBBBB" />
+          ) : (
+            <Text style={styles.normalWhite}>Continue</Text>
+          )}
+        </TouchableOpacity>
+      </View>
+      {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+    </View>
   );
 };
 
