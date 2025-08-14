03b507295fbc32263f8e49ab743746627ed87434
ts336 <22297@apps.eggs.school.nz>
2025-08-14 13:26:25 +1200

    Final - Fixed errors

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'; // for write to file
 import React, { useEffect, useState } from 'react'; // for hooks
-import { ActivityIndicator, Alert, Button, Image, ImageBackground, Text, TextInput, TouchableOpacity, View } from 'react-native'; // basic components of React Native
+import { ActivityIndicator, Alert, Button, Image, ImageBackground, Keyboard, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'; // basic components of React Native
 import DropDownPicker from 'react-native-dropdown-picker';
 import styles from './styles'; // importing stylesheet
 
@@ -15,7 +15,6 @@ const months = [
   { label: 'September', value: 'September' }, { label: 'October', value: 'October' },
   { label: 'November', value: 'November' }, { label: 'December', value: 'December' }
 ];
-const days = Array.from({ length: 31 }, (_, i) => ({ label: (i + 1).toString(), value: i + 1 }));
 const times = [
   { label: '09:00', value: '09:00' }, { label: '10:00', value: '10:00' },
   { label: '11:00', value: '11:00' }, { label: '13:00', value: '13:00' },
@@ -25,66 +24,68 @@ const times = [
 
 // Login screen, accepts onLoginSuccess prop
 const LoginScreen = ({ onLoginSuccess }) => {
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   
 
   const handleLogin = () => {
-    if (email === '' || password === '') {
-      setErrorMessage('Email and password cannot be empty.');
+    if (username === '' || password === '') {
+      setErrorMessage('Username and password cannot be empty.');
     } else {
       // Login logic
-      if (email === fixedUsername && password === fixedPassword) {
+      if (username === fixedUsername && password === fixedPassword) {
         setErrorMessage(''); // Clear any previous error messages
         
         // Call the function to signal that the login was successful
         onLoginSuccess();
 
-      } else { //Login will only work for set email and password
-        setErrorMessage('Invalid email or password.');
+      } else { //Login will only work for set username and password
+        setErrorMessage('Invalid username or password.');
       }
     }
   };
 
-  const isEnabled = email.length > 0 && password.length > 0; // Enables button when there is input
+  const isEnabled = username.length > 0 && password.length > 0; // Enables button when there is input
   const buttonStyleLogin = isEnabled ? styles.enabledLoginButton : styles.disabledLoginButton; // ternary operator
 
   return ( //view screen (what will be returned to the screen)
-    <View style={styles.container}>
-      <Text style={styles.title}>Sano</Text>
-      <Text style={styles.slogan}>Healthcare made easy.</Text>
-      <View style={styles.box}>
-        <View style={styles.signInHeadingContainer}>
-          <Text style={styles.signInHeading}>Sign in</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Sano</Text>
+        <Text style={styles.slogan}>Healthcare made easy.</Text>
+        <View style={styles.box}>
+          <View style={styles.signInHeadingContainer}>
+            <Text style={styles.signInHeading}>Sign in</Text>
+          </View>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          /> 
         </View>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        /> 
+        {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+        <TouchableOpacity
+          style={buttonStyleLogin} // conditional style for enabled and disabled
+          onPress={handleLogin}
+          disabled={!isEnabled} // disable button press
+        >
+          <Text style={styles.loginButtonText}>Sign in</Text>
+        </TouchableOpacity>
       </View>
-      {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
-      <TouchableOpacity
-        style={buttonStyleLogin} // conditional style for enabled and disabled
-        onPress={handleLogin}
-        disabled={!isEnabled} // disable button press
-      >
-        <Text style={styles.loginButtonText}>Sign in</Text>
-      </TouchableOpacity>
-    </View> //if there is an error, it will be shown with the errorText style, otherwise nothing. onPress handleLogin is called
+    </TouchableWithoutFeedback>
   );
 };
+
 
 // Home screen, accepts onLogout and onNavigateToInfo props
 const HomeScreen = ({ onLogout, onNavigateToInfo }) => {
@@ -142,6 +143,7 @@ const AppointmentFormScreen = ({ onGoBackToInfo, onNavigateToConfirmBooking }) =
 
   const [openDay, setOpenDay] = useState(false); // controls day dropdown visibility
   const [selectedDay, setSelectedDay] = useState(null);
+  const [dayItems, setDayItems] = useState([]); // for dynamic list
 
   const [openTime, setOpenTime] = useState(false); // controls time dropdown visibility
   const [selectedTime, setSelectedTime] = useState(null);
@@ -154,12 +156,30 @@ const AppointmentFormScreen = ({ onGoBackToInfo, onNavigateToConfirmBooking }) =
   const [isSaving, setIsSaving] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
+  // For days in drop down menu
+  useEffect(() => {
+    let daysInMonth = 31;
+    // Determine the number of days for the selected month
+    if (selectedMonth === 'February') {
+      daysInMonth = 28; // Simple case, not handling leap years
+    } else if (['April', 'June', 'September', 'November'].includes(selectedMonth)) {
+      daysInMonth = 30;
+    }
+    
+    // Generate a new array of days
+    const newDays = Array.from({ length: daysInMonth }, (_, i) => ({ label: (i + 1).toString(), value: i + 1 }));
+    setDayItems(newDays);
+
+    // Reset the selected day if the month changes to avoid invalid dates
+    setSelectedDay(null);
+  }, [selectedMonth]);
+
   // function for validating the name given
   const validateFullName = (name) => {
     setErrorMessage(''); // Clear previous error messages before validation
 
-    // To check for characters that are not letters, spaces or hyphens
-    const invalidCharsRegex = /[^a-zA-Z\s-]/;
+    // To check for characters that are not letters, spaces, apostrophes or hyphens
+    const invalidCharsRegex = /[^a-zA-Z\s-']/;
 
     if (invalidCharsRegex.test(name)) {
       setErrorMessage('Full name can only contain letters, spaces and hyphens.');
@@ -255,172 +275,173 @@ const AppointmentFormScreen = ({ onGoBackToInfo, onNavigateToConfirmBooking }) =
   // if selected appointment type, changes style of button (radio button)
   // continue button disabled if saving or no appointment type has been selected
   return ( 
-    <View style={[styles.container, { justifyContent: 'center', alignItems: 'center', paddingTop: 40}]}>
-      <Text style={[styles.heading, { width: '100%', textAlign: 'center'}]}>Enter your Details</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Full Name"
-          value={fullName}
-          onChangeText={setFullName}
-          keyboardType="email-address"
-          autoCapitalize="words"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Phone Number"
-          value={phoneNumber}
-          onChangeText={setPhoneNumber}
-          keyboardType="numeric"
-        />
-      
-      <View style={styles.line} />
-      
-      <Text style={[styles.heading, { width: '100%', textAlign: 'center' }]}>Appointment Type</Text>
-      <View style={styles.radioGroupContainer}>
-        <TouchableOpacity
-          style={[
-            styles.radioButton,
-            selectedAppointmentType === 'Virtual' && styles.selectedRadioButton,
-          ]}
-          onPress={() => setSelectedAppointmentType('Virtual')}
-        >
-          <Text style={[
-            styles.radioButtonText,
-            selectedAppointmentType === 'Virtual' && styles.selectedRadioButtonText,
-          ]}>
-            Virtual
-          </Text>
-        </TouchableOpacity>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={[styles.container, { justifyContent: 'center', alignItems: 'center', paddingTop: 40}]}>
+        <Text style={[styles.heading, { width: '100%', textAlign: 'center'}]}>Enter your Details</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Full Name"
+            value={fullName}
+            onChangeText={setFullName}
+            autoCapitalize="words"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+            keyboardType="numeric"
+          />
+        
+        <View style={styles.line} />
+        
+        <Text style={[styles.heading, { width: '100%', textAlign: 'center' }]}>Appointment Type</Text>
+        <View style={styles.radioGroupContainer}>
+          <TouchableOpacity
+            style={[
+              styles.radioButton,
+              selectedAppointmentType === 'Virtual' && styles.selectedRadioButton,
+            ]}
+            onPress={() => setSelectedAppointmentType('Virtual')}
+          >
+            <Text style={[
+              styles.radioButtonText,
+              selectedAppointmentType === 'Virtual' && styles.selectedRadioButtonText,
+            ]}>
+              Virtual
+            </Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={[
-            styles.radioButton,
-            selectedAppointmentType === 'In-person' && styles.selectedRadioButton,
-          ]}
-          onPress={() => setSelectedAppointmentType('In-person')}
-        >
-          <Text style={[
-            styles.radioButtonText,
-            selectedAppointmentType === 'In-person' && styles.selectedRadioButtonText,
-          ]}>
-            In-person
-          </Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.radioButton,
+              selectedAppointmentType === 'In-person' && styles.selectedRadioButton,
+            ]}
+            onPress={() => setSelectedAppointmentType('In-person')}
+          >
+            <Text style={[
+              styles.radioButtonText,
+              selectedAppointmentType === 'In-person' && styles.selectedRadioButtonText,
+            ]}>
+              In-person
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.line} />
+
+        <Text style={[styles.heading, { width: '100%', textAlign: 'center'}]}>Select Practitioner</Text>
+        <View style={styles.radioGroupContainer}>
+          <TouchableOpacity
+            style={[
+              styles.radioButton,
+              selectedPractitioner === 'Dr. Bob' && styles.selectedRadioButton,
+            ]}
+            onPress={() => setSelectedPractitioner('Dr. Bob')}
+          >
+            <Text style={[
+              styles.radioButtonText,
+              selectedPractitioner === 'Dr. Bob' && styles.selectedRadioButtonText,
+            ]}>
+              Dr. Bob
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[
+              styles.radioButton,
+              selectedPractitioner === 'Dr. Jane' && styles.selectedRadioButton,
+            ]}
+            onPress={() => setSelectedPractitioner('Dr. Jane')}
+          >
+            <Text style={[
+              styles.radioButtonText,
+              selectedPractitioner === 'Dr. Jane' && styles.selectedRadioButtonText,
+            ]}>
+              Dr. Jane
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.line} />
+
+        <Text style={[styles.heading, { width: '100%', textAlign: 'center'}]}>Select Date and Time</Text>
+        <View style={styles.pickerContainer}>
+          <DropDownPicker
+            open={openMonth}
+            value={selectedMonth}
+            items={months}
+            setOpen={setOpenMonth}
+            setValue={setSelectedMonth}
+            setItems={() => {}} // not filling this in since not needed
+            style={styles.dropdown}
+            textStyle={styles.dropdownText}
+            containerStyle={styles.dropdownContainer}
+            placeholder="Month"
+            // To close other pickers when this one opens
+            onOpen={() => { setOpenDay(false); setOpenTime(false); }}
+            dropDownDirection="TOP" // Prevents overlapping with content below
+            zIndex={3000} // Ensures that this picker is on top
+            listMode="SCROLLVIEW"
+          />
+
+          <DropDownPicker
+            open={openDay}
+            value={selectedDay}
+            items={dayItems} // changed for dyanamic listing
+            setOpen={setOpenDay}
+            setValue={setSelectedDay}
+            setItems={() => {}}
+            style={styles.dropdown}
+            textStyle={styles.dropdownText}
+            containerStyle={styles.dropdownContainer}
+            placeholder="Day"
+            // Close other pickers when this one opens
+            onOpen={() => { setOpenMonth(false); setOpenTime(false); }}
+            dropDownDirection="TOP"
+            zIndex={2000} // Lower zIndex than month
+            listMode="SCROLLVIEW"
+          />
+
+          <DropDownPicker
+            open={openTime}
+            value={selectedTime}
+            items={times}
+            setOpen={setOpenTime}
+            setValue={setSelectedTime}
+            setItems={() => {}}
+            style={styles.dropdown}
+            textStyle={styles.dropdownText}
+            containerStyle={styles.dropdownContainer}
+            placeholder="Time"
+            // Close other pickers when this one opens
+            onOpen={() => { setOpenMonth(false); setOpenDay(false); }}
+            dropDownDirection="TOP"
+            zIndex={1000} // Lowest zIndex
+            listMode="SCROLLVIEW"
+          />
+        </View>
+        
+        {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.backButton} onPress={onGoBackToInfo}>
+            <Text style={styles.normalWhite}>Back</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={buttonStyleForm}
+            onPress={handleSaveAppointment}
+            disabled={isSaving || !selectedAppointmentType}
+          >
+            {isSaving ? (
+              <ActivityIndicator color="#4DBBBB" />
+            ) : (
+              <Text style={styles.normalWhite}>Continue</Text>
+            )}
+          </TouchableOpacity>
+        </View>
       </View>
-
-      <View style={styles.line} />
-
-      <Text style={[styles.heading, { width: '100%', textAlign: 'center'}]}>Select Practitioner</Text>
-      <View style={styles.radioGroupContainer}>
-        <TouchableOpacity
-          style={[
-            styles.radioButton,
-            selectedPractitioner === 'Dr. Bob' && styles.selectedRadioButton,
-          ]}
-          onPress={() => setSelectedPractitioner('Dr. Bob')}
-        >
-          <Text style={[
-            styles.radioButtonText,
-            selectedPractitioner === 'Dr. Bob' && styles.selectedRadioButtonText,
-          ]}>
-            Dr. Bob
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={[
-            styles.radioButton,
-            selectedPractitioner === 'Dr. Jane' && styles.selectedRadioButton,
-          ]}
-          onPress={() => setSelectedPractitioner('Dr. Jane')}
-        >
-          <Text style={[
-            styles.radioButtonText,
-            selectedPractitioner === 'Dr. Jane' && styles.selectedRadioButtonText,
-          ]}>
-            Dr. Jane
-          </Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.line} />
-
-      <Text style={[styles.heading, { width: '100%', textAlign: 'center'}]}>Select Date and Time</Text>
-      <View style={styles.pickerContainer}>
-        <DropDownPicker
-          open={openMonth}
-          value={selectedMonth}
-          items={months}
-          setOpen={setOpenMonth}
-          setValue={setSelectedMonth}
-          setItems={() => {}} // not filling this in since not needed
-          style={styles.dropdown}
-          textStyle={styles.dropdownText}
-          containerStyle={styles.dropdownContainer}
-          placeholder="Month"
-          // To close other pickers when this one opens
-          onOpen={() => { setOpenDay(false); setOpenTime(false); }}
-          dropDownDirection="TOP" // Prevents overlapping with content below
-          zIndex={3000} // Ensures that this picker is on top
-          listMode="SCROLLVIEW"
-        />
-
-        <DropDownPicker
-          open={openDay}
-          value={selectedDay}
-          items={days}
-          setOpen={setOpenDay}
-          setValue={setSelectedDay}
-          setItems={() => {}}
-          style={styles.dropdown}
-          textStyle={styles.dropdownText}
-          containerStyle={styles.dropdownContainer}
-          placeholder="Day"
-          // Close other pickers when this one opens
-          onOpen={() => { setOpenMonth(false); setOpenTime(false); }}
-          dropDownDirection="TOP"
-          zIndex={2000} // Lower zIndex than month
-          listMode="SCROLLVIEW"
-        />
-
-        <DropDownPicker
-          open={openTime}
-          value={selectedTime}
-          items={times}
-          setOpen={setOpenTime}
-          setValue={setSelectedTime}
-          setItems={() => {}}
-          style={styles.dropdown}
-          textStyle={styles.dropdownText}
-          containerStyle={styles.dropdownContainer}
-          placeholder="Time"
-          // Close other pickers when this one opens
-          onOpen={() => { setOpenMonth(false); setOpenDay(false); }}
-          dropDownDirection="TOP"
-          zIndex={1000} // Lowest zIndex
-          listMode="SCROLLVIEW"
-        />
-      </View>
-      
-      {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.backButton} onPress={onGoBackToInfo}>
-          <Text style={styles.normalWhite}>Back</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={buttonStyleForm}
-          onPress={handleSaveAppointment}
-          disabled={isSaving || !selectedAppointmentType}
-        >
-          {isSaving ? (
-            <ActivityIndicator color="#4DBBBB" />
-          ) : (
-            <Text style={styles.normalWhite}>Continue</Text>
-          )}
-        </TouchableOpacity>
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
